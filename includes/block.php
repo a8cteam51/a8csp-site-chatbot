214b6e65313fbd968a166e5d33be4a2767a72e32
Esteban Cairol <ecairol@gmail.com>
2025-12-19 14:37:47 -0600

    Replaced the chat button with an SVG icon for improved visual consistency.

--- includes/block.php
+++ includes/block.php
@@ -255,7 +255,13 @@ function a8csp_render_site_chatbot_block( $attributes ) {
 		</div>
 		<form id="a8csp-chat-form">
 			<textarea id="a8csp-chat-input" placeholder="Type your message..."></textarea>
-			<button type="submit"><span class="dashicons dashicons-arrow-up-alt"></span></button>
+			<button type="submit">
+				<svg
+					viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" 
+					width="36" height="36" fill="currentColor" 
+					aria-hidden="true"><path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+				</svg>
+			</button>
 		</form>
 	</div>
 	<?php
